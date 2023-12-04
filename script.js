23cfb3318a1b50d9f46086f9273feef67925454d
Farhan Nazmi Faridwajdi <54557198+farhannazmi11@users.noreply.github.com>
2023-12-04 12:38:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you next year! I'll pick you up my passenger princess";
+  question.innerHTML = "Yay, see you next year! I'll pick you up my passenger princess.";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
